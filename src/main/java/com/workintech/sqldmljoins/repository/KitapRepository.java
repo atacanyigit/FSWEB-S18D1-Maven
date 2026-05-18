713608ce53d2a3197710dd155a68c604eb3f6468
Yiğit GÜLEÇ <githubda-kullandigin-email@example.com>
2026-05-18 20:58:10 +0300

    fix: resolve locale format issue in tests

---
+++ src/main/java/com/workintech/sqldmljoins/repository/KitapRepository.java
@@ -0,0 +1,21 @@
+package com.workintech.sqldmljoins.repository;
+
+import com.workintech.sqldmljoins.entity.Kitap;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface KitapRepository extends JpaRepository<Kitap, Long> {
+
+    //Dram ve Hikaye türündeki kitapları listeleyin. JOIN kullanmadan yapın.
+    String QUESTION_1 = "SELECT k.* FROM kitap k, tur t WHERE k.turno = t.turno AND t.ad IN('Hikaye' , 'Dram')";
+    @Query(value = QUESTION_1, nativeQuery = true)
+    List<Kitap> findBooks();
+
+    //Tüm kitapların ortalama puanını bulunuz.
+    String QUESTION_10 = "SELECT AVG(k.puan) FROM kitap k";
+    @Query(value = QUESTION_10, nativeQuery = true)
+    Double findAvgPointOfBooks();
+
+}
